4ddd8a5d8c364ad117b1f15e69f860306d08a077
brandpost1 <viktor.oberg@gmail.com>
2021-12-18 09:14:01 +0100

    Removed some stuff left behind, and some refactoring.

--- 2021/Day14.cs
+++ 2021/Day14.cs
@@ -3,262 +3,268 @@ using System.Text;
 
 namespace _2021
 {
-    internal class Day14
-    {
-        /*
-        private static string PolymerTemplate = "NNCB";
-        private static Dictionary<string, string> TokenMap = new()
-        {
-            { "CH", "CBH" },
-            { "HH", "HNH" },
-            { "CB", "CHB" },
-            { "NH", "NCH" },
-            { "HB", "HCB" },
-            { "HC", "HBC" },
-            { "HN", "HCN" },
-            { "NN", "NCN" },
-            { "BH", "BHH" },
-            { "NC", "NBC" },
-            { "NB", "NBB" },
-            { "BN", "BBN" },
-            { "BB", "BNB" },
-            { "BC", "BBC" },
-            { "CC", "CNC" },
-            { "CN", "CCN" }
-        };
-        
-        */
-        private static string PolymerTemplate = "SVKVKCCBNHNSOSCCOPOC";
-        private static Dictionary<string, string> TokenMap = new()
-        {
-            { "KK", "KBK" },
-            { "CS", "CPS" },
-            { "VV", "VOV" },
-            { "KO", "KSO" },
-            { "PO", "PNO" },
-            { "PH", "PKH" },
-            { "BV", "BOV" },
-            { "VH", "VVH" },
-            { "PF", "PPF" },
-            { "HB", "HBB" },
-            { "OB", "OVB" },
-            { "FC", "FFC" },
-            { "OS", "OHS" },
-            { "NB", "NPB" },
-            { "SH", "SSH" },
-            { "KV", "KKV" },
-            { "SO", "SCO" },
-            { "NP", "NBP" },
-            { "NV", "NFV" },
-            { "CP", "COP" },
-            { "KS", "KNS" },
-            { "FP", "FBP" },
-            { "VN", "VVN" },
-            { "NC", "NSC" },
-            { "FH", "FNH" },
-            { "CB", "CVB" },
-            { "PV", "PBV" },
-            { "NH", "NBH" },
-            { "NF", "NHF" },
-            { "PC", "PBC" },
-            { "NO", "NNO" },
-            { "CN", "CPN" },
-            { "KF", "KBF" },
-            { "VF", "VSF" },
-            { "CC", "CKC" },
-            { "CF", "CNF" },
-            { "PS", "PSS" },
-            { "NK", "NNK" },
-            { "PB", "PHB" },
-            { "BP", "BOP" },
-            { "FK", "FNK" },
-            { "BO", "BSO" },
-            { "OH", "OCH" },
-            { "VB", "VSB" },
-            { "VP", "VFP" },
-            { "FO", "FVO" },
-            { "KB", "KCB" },
-            { "SK", "SHK" },
-            { "CO", "CHO" },
-            { "HV", "HHV" },
-            { "SV", "SBV" },
-            { "BF", "BOF" },
-            { "SS", "SKS" },
-            { "VK", "VSK" },
-            { "HS", "HBS" },
-            { "HF", "HPF" },
-            { "PK", "PFK" },
-            { "BS", "BOS" },
-            { "BB", "BOB" },
-            { "VC", "VPC" },
-            { "OP", "OFP" },
-            { "NS", "NPS" },
-            { "SB", "SCB" },
-            { "NN", "NKN" },
-            { "HC", "HSC" },
-            { "HH", "HBH" },
-            { "FN", "FPN" },
-            { "OO", "OVO" },
-            { "VO", "VNO" },
-            { "ON", "OPN" },
-            { "FV", "FKV" },
-            { "HK", "HSK" },
-            { "FS", "FVS" },
-            { "HO", "HVO" },
-            { "PN", "PBN" },
-            { "KH", "KBH" },
-            { "CH", "CCH" },
-            { "KP", "KSP" },
-            { "BH", "BOH" },
-            { "BK", "BBK" },
-            { "FB", "FHB" },
-            { "VS", "VSS" },
-            { "HP", "HOP" },
-            { "SP", "SPP" },
-            { "OV", "OFV" },
-            { "OF", "OHF" },
-            { "OC", "OVC" },
-            { "KN", "KHN" },
-            { "BC", "BFC" },
-            { "BN", "BFN" },
-            { "CK", "CKK" },
-            { "SN", "SPN" },
-            { "SF", "SKF" },
-            { "KC", "KCC" },
-            { "SC", "SCC" },
-            { "HN", "HVN" },
-            { "OK", "OOK" },
-            { "FF", "FVF" },
-            { "CV", "CVV" },
-            { "PP", "PVP" },
-        };
-        
-        private IEnumerable<string> SplitToTokens(string polymer)
-        {
-            for (int i = 0; i < polymer.Length - 1; i++)
-                yield return polymer.Substring(i, 2);
-        }
+  internal class Day14
+  {
+	/*
+	private static string PolymerTemplate = "NNCB";
+	private static Dictionary<string, string> TokenMap = new()
+	{
+		{ "CH", "CBH" },
+		{ "HH", "HNH" },
+		{ "CB", "CHB" },
+		{ "NH", "NCH" },
+		{ "HB", "HCB" },
+		{ "HC", "HBC" },
+		{ "HN", "HCN" },
+		{ "NN", "NCN" },
+		{ "BH", "BHH" },
+		{ "NC", "NBC" },
+		{ "NB", "NBB" },
+		{ "BN", "BBN" },
+		{ "BB", "BNB" },
+		{ "BC", "BBC" },
+		{ "CC", "CNC" },
+		{ "CN", "CCN" }
+	};
+	*/
+	
+	private static string PolymerTemplate = "SVKVKCCBNHNSOSCCOPOC";
+	private static Dictionary<string, string> TokenMap = new()
+	{
+	  { "KK", "KBK" },
+	  { "CS", "CPS" },
+	  { "VV", "VOV" },
+	  { "KO", "KSO" },
+	  { "PO", "PNO" },
+	  { "PH", "PKH" },
+	  { "BV", "BOV" },
+	  { "VH", "VVH" },
+	  { "PF", "PPF" },
+	  { "HB", "HBB" },
+	  { "OB", "OVB" },
+	  { "FC", "FFC" },
+	  { "OS", "OHS" },
+	  { "NB", "NPB" },
+	  { "SH", "SSH" },
+	  { "KV", "KKV" },
+	  { "SO", "SCO" },
+	  { "NP", "NBP" },
+	  { "NV", "NFV" },
+	  { "CP", "COP" },
+	  { "KS", "KNS" },
+	  { "FP", "FBP" },
+	  { "VN", "VVN" },
+	  { "NC", "NSC" },
+	  { "FH", "FNH" },
+	  { "CB", "CVB" },
+	  { "PV", "PBV" },
+	  { "NH", "NBH" },
+	  { "NF", "NHF" },
+	  { "PC", "PBC" },
+	  { "NO", "NNO" },
+	  { "CN", "CPN" },
+	  { "KF", "KBF" },
+	  { "VF", "VSF" },
+	  { "CC", "CKC" },
+	  { "CF", "CNF" },
+	  { "PS", "PSS" },
+	  { "NK", "NNK" },
+	  { "PB", "PHB" },
+	  { "BP", "BOP" },
+	  { "FK", "FNK" },
+	  { "BO", "BSO" },
+	  { "OH", "OCH" },
+	  { "VB", "VSB" },
+	  { "VP", "VFP" },
+	  { "FO", "FVO" },
+	  { "KB", "KCB" },
+	  { "SK", "SHK" },
+	  { "CO", "CHO" },
+	  { "HV", "HHV" },
+	  { "SV", "SBV" },
+	  { "BF", "BOF" },
+	  { "SS", "SKS" },
+	  { "VK", "VSK" },
+	  { "HS", "HBS" },
+	  { "HF", "HPF" },
+	  { "PK", "PFK" },
+	  { "BS", "BOS" },
+	  { "BB", "BOB" },
+	  { "VC", "VPC" },
+	  { "OP", "OFP" },
+	  { "NS", "NPS" },
+	  { "SB", "SCB" },
+	  { "NN", "NKN" },
+	  { "HC", "HSC" },
+	  { "HH", "HBH" },
+	  { "FN", "FPN" },
+	  { "OO", "OVO" },
+	  { "VO", "VNO" },
+	  { "ON", "OPN" },
+	  { "FV", "FKV" },
+	  { "HK", "HSK" },
+	  { "FS", "FVS" },
+	  { "HO", "HVO" },
+	  { "PN", "PBN" },
+	  { "KH", "KBH" },
+	  { "CH", "CCH" },
+	  { "KP", "KSP" },
+	  { "BH", "BOH" },
+	  { "BK", "BBK" },
+	  { "FB", "FHB" },
+	  { "VS", "VSS" },
+	  { "HP", "HOP" },
+	  { "SP", "SPP" },
+	  { "OV", "OFV" },
+	  { "OF", "OHF" },
+	  { "OC", "OVC" },
+	  { "KN", "KHN" },
+	  { "BC", "BFC" },
+	  { "BN", "BFN" },
+	  { "CK", "CKK" },
+	  { "SN", "SPN" },
+	  { "SF", "SKF" },
+	  { "KC", "KCC" },
+	  { "SC", "SCC" },
+	  { "HN", "HVN" },
+	  { "OK", "OOK" },
+	  { "FF", "FVF" },
+	  { "CV", "CVV" },
+	  { "PP", "PVP" },
+	};
+	
+	private IEnumerable<string> SplitToTokens(string polymer)
+	{
+	  for (int i = 0; i < polymer.Length - 1; i++)
+		yield return polymer.Substring(i, 2);
+	}
 
 
-        public class Node
-        {
-            public Node(string name, bool isRootNode = false)
-            {
-                _name = name;
-                _isRootNode = isRootNode;
-            }
-            public string _name { get; set; } = "";
-            public bool _isRootNode { get; set;}
+	public class Node
+	{
+	  public Node(string name, bool isRootNode = false)
+	  {
+		Name = name;
+		IsRootNode = isRootNode;
+	  }
+	  public string Name { get; set; } = "";
+	  public bool IsRootNode { get; set; }
 
-            public Dictionary<char, long> _leafData = new();
-            public bool _isLeftmostLeafParent { get; set; } = false;
+	  public Dictionary<char, long> LeafData = new();
 
-            public override string ToString()
-            {
-                return _name;
-            }
+	  public override string ToString()
+	  {
+		return Name;
+	  }
 
-            internal void PopulateLeafDataFrom(Node child)
-            {
-                var left = child._name[0];
-                var right = child._name[1];
+	  public void PopulateLeafDataFrom(Node child)
+	  {
+		var left = child.Name[0];
+		var right = child.Name[1];
 
-                if (!_leafData.ContainsKey(left))
-                    _leafData.Add(left, 0);
+		if (!LeafData.ContainsKey(left))
+		  LeafData.Add(left, 0);
 
-                if (!_leafData.ContainsKey(right))
-                    _leafData.Add(right, 0);
+		if (!LeafData.ContainsKey(right))
+		  LeafData.Add(right, 0);
 
-                _leafData[right] += 1;
-            }
+		LeafData[right] += 1;
+	  }
 
-            internal void PropagateLeafDataFrom(Node child, int depth, bool leftmostPath)
-            {
-                foreach (var item in child._leafData)
-                {
-                    if (!_leafData.ContainsKey(item.Key))
-                        _leafData.Add(item.Key, 0);
-                    if (!_leafData.ContainsKey(item.Key))
-                        _leafData.Add(item.Key, 0);
+	  public void PropagateLeafDataFrom(Node child)
+	  {
+		foreach (var item in child.LeafData)
+		{
+		  if (!LeafData.ContainsKey(item.Key))
+			LeafData.Add(item.Key, 0);
+		  if (!LeafData.ContainsKey(item.Key))
+			LeafData.Add(item.Key, 0);
 
-                    _leafData[item.Key] += item.Value;
-                }
-            }
-        }
 
-        public void Run()
-        {
-            var sw = new Stopwatch();
-            sw.Start();
-            var root = new Node(PolymerTemplate, true);
+		  LeafData[item.Key] += item.Value;
+		}
+	  }
+	}
 
-            Expand(root, 41); // Equal to 40
-            root._leafData[PolymerTemplate[0]] += 1; // Add the leftmost leaf of the tree
-            
-            var ordered = root._leafData.OrderBy(x => x.Value);
-            foreach (var item in ordered)
-            {
-                Console.WriteLine($"{item.Key}: {item.Value}");
-            }
-            sw.Stop();
+	public void Run()
+	{
+	  var sw = new Stopwatch();
+	  sw.Start();
+	  var root = new Node(PolymerTemplate, true);
 
-            Console.WriteLine();
-            Console.WriteLine($"Result is: {ordered.Last().Value - ordered.First().Value}");
-            Console.WriteLine();
-            Console.WriteLine($"Elapsed: {sw.ElapsedMilliseconds}ms");
-        }
+	  Expand(root, 41); // Equal to 40
 
-        private Dictionary<string, Dictionary<int, Node>> _nodeMap = new();
+	  var ordered = root.LeafData.OrderBy(x => x.Value);
+	  foreach (var item in ordered)
+	  {
+		Console.WriteLine($"{item.Key}: {item.Value}");
+	  }
+	  sw.Stop();
 
-        private void Expand(Node parent, int depth, bool propagatedLeftmostPath = true)
-        {
-            if (depth == 0)
-                return;
+	  Console.WriteLine();
+	  Console.WriteLine($"Result is: {ordered.Last().Value - ordered.First().Value}");
+	  Console.WriteLine();
+	  Console.WriteLine($"Elapsed: {sw.ElapsedMilliseconds}ms");
+	}
 
-            string[] splitMappedToken;
-            var mappedToken = parent._name;
+	private Dictionary<string, Dictionary<int, Node>> _nodeMap = new();
 
-            if (TokenMap.ContainsKey(parent._name))
-                mappedToken = TokenMap[parent._name];
+	private void Expand(Node parent, int depth)
+	{
+	  if (depth == 0)
+		return;
 
-            splitMappedToken = SplitToTokens(mappedToken).ToArray();
+	  string[] splitMappedToken;
+	  var mappedToken = parent.Name;
 
-            for (int i = 0; i < splitMappedToken.Count(); i++)
-            {
-                var token = splitMappedToken[i];
-                var leftMostPath = i == 0 && propagatedLeftmostPath;
+	  if (TokenMap.ContainsKey(parent.Name))
+		mappedToken = TokenMap[parent.Name];
 
-                Dictionary<int, Node> nodeMap;
-                if(!_nodeMap.TryGetValue(token, out nodeMap))
-                {
-                    nodeMap = new();
-                    _nodeMap[token] = nodeMap;
-                }
+	  splitMappedToken = SplitToTokens(mappedToken).ToArray();
 
-                var nextDepth = depth - 1;
-                var nodeFound = nodeMap.TryGetValue(depth, out var node);
-                
-                if (nodeFound && node._leafData.Any())
-                {
-                    parent.PropagateLeafDataFrom(node, depth, leftMostPath);
-                    continue;
-                }
+	  for (int i = 0; i < splitMappedToken.Count(); i++)
+	  {
+		var token = splitMappedToken[i];
+		var rootLeftmostPath = i == 0 && parent.IsRootNode;
 
-                node = new(token);
+		Dictionary<int, Node> nodeMap;
+		if (!_nodeMap.TryGetValue(token, out nodeMap))
+		{
+		  nodeMap = new();
+		  _nodeMap[token] = nodeMap;
+		}
 
-                if (nextDepth != 0)
-                    nodeMap[depth] = node;
-                
-                Expand(node, nextDepth, leftMostPath);
+		var nextDepth = depth - 1;
+		var nodeFound = nodeMap.TryGetValue(depth, out var node);
 
-                if (node._isLeftmostLeafParent) // May have been set in Expand() -> PopulateLeafDataFrom()
-                    nodeMap.Remove(depth);
+		if (nodeFound && node.LeafData.Any())
+		{
+		  parent.PropagateLeafDataFrom(node);
+		  continue;
+		}
 
-                if (nextDepth == 0)
-                    parent.PopulateLeafDataFrom(node);
-                else
-                    parent.PropagateLeafDataFrom(node, depth, leftMostPath);
-            }
-        }
-    }
+		node = new(token);
+
+		if (nextDepth != 0)
+		  nodeMap[depth] = node;
+
+		Expand(node, nextDepth);
+
+		// First leaf data should only be included when propagated up the leftmost path of the tree
+		if (rootLeftmostPath)
+		{
+		  if (node.LeafData.ContainsKey(parent.Name[0]))
+		  {
+			node.LeafData[parent.Name[0]] += 1;
+		  }
+		}
+
+		if (nextDepth == 0)
+		  parent.PopulateLeafDataFrom(node);
+		else
+		  parent.PropagateLeafDataFrom(node);
+
+	  }
+	}
+  }
 }
